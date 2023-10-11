e4ae7e2428949ae635612aa394b41aff464aa79c
Rakib Hossen <rakibhossen1660@gmail.com>
2023-10-11 14:47:33 +0600

    create video playlist home page

---
+++ src/components/VideoCourseList.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import GlobalAPI from "../shared/GlobalAPI";
+
+const VideoCourseList = () => {
+  const [videolist, setVideolist] = useState([]);
+  // const navigation = useNaviga
+  useEffect(() => {
+    getVideoCourse();
+  }, []);
+
+  const getVideoCourse = async () => {
+    const resp = (await GlobalAPI.getVideoCourse()).data;
+    const result = resp.data.map((item) => ({
+      id: item.id,
+      name: item.attributes.title,
+      description: item.attributes.description,
+      image: item.attributes.image.data.attributes.url,
+      Topic: item.attributes.VideoTopic,
+    }));
+    setVideolist(result);
+    console.log(result);
+  };
+  return (
+    <View style={{marginTop: 15}}>
+      <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 3 }}>
+        Video Course
+      </Text>
+      <FlatList
+        data={videolist}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <Image
+              source={{ uri: item.image }}
+              style={{
+                width: 210,
+                height: 120,
+                marginRight: 10,
+                borderRadius: 7,
+              }}
+            />
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+};
+
+export default VideoCourseList;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { Text, View } from "react-native";
 import WelcomeHeader from "../components/WelcomeHeader";
 import SearchBar from "../components/SearchBar";
 import Slider from "../components/Slider";
+import VideoCourseList from "../components/VideoCourseList";
 
 const Home = () =>{
     return(
@@ -10,6 +11,7 @@ const Home = () =>{
             <WelcomeHeader />
             <SearchBar />
             <Slider />
+            <VideoCourseList />
         </View>
     )
 }
